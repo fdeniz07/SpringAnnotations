273e1bf3471905215417b339f804c2611e9ad1b7
Fatih Deniz <deniz.fatih@hotmail.de>
2023-04-12 01:33:26 +0200

    Spring Annotation -2

--- src/main/java/com/tpe/repository/DbRepository.java
+++ src/main/java/com/tpe/repository/DbRepository.java
@@ -1,11 +1,13 @@
 package com.tpe.repository;
 
 import com.tpe.domain.Message;
+import org.springframework.stereotype.Component;
 
-public class DbRepository implements Repo{
+@Component
+public class DbRepository implements Repo {
 
     @Override
     public void save(Message message) {
-
+        System.out.println("Mesajiniz DB'ye kaydediliyor. Mesajiniz : " + message.getMessage());
     }
 }
