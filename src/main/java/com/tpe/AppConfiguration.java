ffe25326bb02cc27240672fab4102f774cd05d34
Fatih Deniz <deniz.fatih@hotmail.de>
2023-04-12 01:33:34 +0200

    Spring Annotation -2

--- src/main/java/com/tpe/AppConfiguration.java
+++ src/main/java/com/tpe/AppConfiguration.java
@@ -1,11 +1,18 @@
 package com.tpe;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Random;
 
 @Configuration//bu classta config olacak
 @ComponentScan("com.tpe")//bu packagedaki componentları tara, default: "com.tpe"
 public class AppConfiguration {
 
+    @Bean //Bean annotation ile ben bu nesneden bir tane bean olusturup IOC containere ekleyeyim, birisinin ihtiyaci oldugunda cagirir kullanirim der.
+    public Random random(){
+        return new Random();
+    }
 
 }
